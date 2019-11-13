a949e9a92c8252a0765c929abb2b1bdc076395f5
Adam Zielinski <github@maosmurf.com>
2019-11-13 16:51:14 +0100

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, {label} from './App';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+beforeEach(() => {
+    configure({ adapter: new Adapter() });
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the heading', () => {
+    let wrapper = shallow(<App/>);
+    expect(wrapper.find('h1').text()).toBe('Hello WORLD');
+});
+
+it('generates a label', () => {
+   const l = label('world');
+   expect(l).toBe('Hello WORLD');
 });
