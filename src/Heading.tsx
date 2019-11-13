2f325b66aab97cab120bbca00b19b7baa40e15c6
Adam Zielinski <github@maosmurf.com>
2019-11-13 18:11:01 +0100

    Heading as Class Component

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,12 +1,16 @@
-import React, {FC} from "react";
+import React, {Component} from "react";
 
 type HeadingProps = { name: string };
-
 
 export const label = (name: string) => {
   return `Hello ${name.toUpperCase()}`;
 };
 
-const Heading: FC<HeadingProps> = (props: HeadingProps) => <h1 className={'headerOne'}>{label(props.name)}</h1>;
+class Heading extends Component<HeadingProps> {
+  render() {
+    return <h1
+      className={'headerOne'}>{label(this.props.name)}</h1>;
+  }
+}
 
 export default Heading;
